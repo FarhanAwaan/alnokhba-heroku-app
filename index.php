9a9607b9e997487175c6265b411ef2a2dbd9e006
FarhanAwaan <32543796+FarhanAwaan@users.noreply.github.com>
2021-02-18 11:54:32 +0500

    testing heroku server v2

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-print_r($_SERVER); exit;
+echo "<pre>"; print_r($_SERVER); exit;
 
 include '_bootstrap.php';
 ?>
